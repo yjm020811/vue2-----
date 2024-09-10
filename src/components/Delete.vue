e78f91de596ab53067daffcc46a8cc1a15457311
yjm020811 <2806654030@qq.com>
2024-09-10 16:29:50 +0800

    emit事件执行时机

--- src/components/Delete.vue
+++ src/components/Delete.vue
@@ -2,26 +2,31 @@
   <div>
     <h3>当前最新的count值为:{{ $store.state.count }}</h3>
     <button @click="delete1">减一</button>
-
+    <testDemo @refresh="onRefresh" />
   </div>
 </template>
-  
-<script>
-import { mapMutations } from 'vuex'
-export default {
-  name: 'MyDelete',
-  data() {
-    return {
 
-    }
+<script>
+import { mapMutations } from "vuex";
+import testDemo from "./testDemo.vue";
+export default {
+  name: "MyDelete",
+  components: {
+    testDemo
+  },
+  data() {
+    return {};
   },
   methods: {
     //  delete1(){
     //   this.$store.commit('delete')
     //  }
-    ...mapMutations({ delete1: "delete" })
+    ...mapMutations({ delete1: "delete" }),
+    onRefresh(e) {
+      console.log("刷新了", e);
+    }
   }
-}
+};
 </script>
-  
+
 <style></style>
---
+++ src/components/testDemo.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <button @click="getData">emit事件test</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {},
+  data() {
+    return {};
+  },
+  methods: {
+    getData() {
+      new Promise((resolve) => {
+        setTimeout(() => {
+          resolve([
+            { name: "张三", age: 18 },
+            { name: "李四", age: 19 },
+            { name: "王五", age: 20 }
+          ]);
+        }, 3000);
+      }).then((res) => {
+        console.log(res);
+        this.$emit("refresh", "子组件获取数据完毕");
+      });
+    }
+  }
+};
+</script>
+<style lang="less" scoped></style>
